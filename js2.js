4248464c954cfd09599a6eb11acf4518e729bffc
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2023-07-20 01:52:06 +0000

    new file:   js2.js

---
+++ js2.js
@@ -0,0 +1,8 @@
+function sui(a, b){
+    var a = window.prompt("Enter first number");
+    var b = window.prompt("Enter second number");
+    var c = a + b;
+    return c;
+}
+var s = sui(a,b);
+window.alert("sum " + s);
