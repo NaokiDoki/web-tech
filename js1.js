ddf7d1e26f4b16a665b2ce6df63fa876809a07bd
Prasiddha Grg <103157685+NaokiDoki@users.noreply.github.com>
2023-07-20 01:34:24 +0000

    new file:   js1.js 	modified:   web.html

---
+++ js1.js
@@ -0,0 +1,14 @@
+function sui(){
+    var a = window.prompt("Enter a number");
+    
+    if (a<0){
+        window.alert("the number is negative");
+    }
+    elseif (a>0){
+        window.alert("the number is positive")
+    }
+    else{
+        window.alert("the number is 0");
+    }
+}
+sui();
